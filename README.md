b9edf9b137009c436657dc9a704d7dc607cb56ee
KRawl <kieran.grain@gmail.com>
2022-10-06 13:06:26 +0100

    Complete code

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # Mixed_Messages
+
+## General info
+- 
+Script to take 3 randomised inputs and create a made up animal fact.
+
+## Technologies
+Project is created with:
+* JavaScript
+* Node
+
+
--- script.js
+++ script.js
@@ -7,3 +7,29 @@ const madeUpAnimalFacts = {
     habitats: ['jungles', 'deserts', 'grassy plains', 'caves', 'rivers', 'houses'],
     food: ['bananas', 'meat', 'grass', 'algae', 'berries', 'insects']
 }
+
+let newAnimalFact = [];
+
+for (let i = 0; i < madeUpAnimalFacts.length; i++) {
+    let arrIndex = generateRandNum(madeUpAnimalFacts[i]);
+    console.log(i);
+
+    if(i === animal) {
+        newAnimalFact.push(`${madeUpAnimalFacts[i][arrIndex]} live in `);
+        return newAnimalFact;
+    } else if (i === habitats) {
+        newAnimalFact.push(`${madeUpAnimalFacts[i][arrIndex]} and eat `);
+    } else if (i === food) {
+        newAnimalFact.push(`${madeUpAnimalFacts[i][arrIndex]}.`);
+    }
+}
+
+function arrToString(arrName) 
+{
+    const strAnimalFact = arrName.join('')
+    return strAnimalFact;
+}
+  
+arrToString(newAnimalFact);
+
+
