ef063049d8822c8d8858b6a24a9bb8878117459e
KRawl <kieran.grain@gmail.com>
2022-10-06 12:07:32 +0100

    Added madeUpAnumalFacts

---
+++ script.js
@@ -0,0 +1,9 @@
+function generateRandNum(arr) {
+    return Math.floor(Math.random() * arr.length);
+}
+
+const madeUpAnimalFacts = {
+    animal: ['Monkeys', 'Dogs', 'Elephants', 'Fish', 'Birds', 'Turtles'],
+    habitats: ['jungles', 'deserts', 'grassy plains', 'caves', 'rivers', 'houses'],
+    food: ['bananas', 'meat', 'grass', 'algae', 'berries', 'insects']
+}
